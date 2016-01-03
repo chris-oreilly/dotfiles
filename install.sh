43c4ca1c65382391062083801583fb662ed01a81
Chris O'Reilly <oreillc194@potsdam.edu>
2016-01-03 15:47:08 -0500

    Fix install.sh
    Using relative filenames for ln won't work in general.

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-HERE=$(dirname $0)
+cd $(dirname $0)
+HERE=$(pwd)
 
 FILES=".gitconfig .gitignore_global"
 
 # create symlinks for each file in links.txt
 for file in $FILES; do
-    ln -sf -T $HERE/$file $HOME/$file
-    echo $file
+    ln -sfv -T $HERE/$file $HOME/$file
 done
